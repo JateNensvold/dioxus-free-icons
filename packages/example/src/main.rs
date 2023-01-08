d74f4abb8c0f141af0a3c47bea777b02a2ec822e
Nate Jensvold <jensvoldnate@gmail.com>
2023-01-07 21:29:10 -0800

    Upgraded dioxus to 0.3.1

--- packages/example/src/main.rs
+++ packages/example/src/main.rs
@@ -3,12 +3,13 @@ use dioxus::prelude::*;
 use dioxus_free_icons::icons::fa_brands_icons::FaRust;
 use dioxus_free_icons::Icon;
 
+
 fn main() {
     // init debug tool for WebAssembly
     wasm_logger::init(wasm_logger::Config::default());
     console_error_panic_hook::set_once();
 
-    dioxus::web::launch(app);
+    dioxus_web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
